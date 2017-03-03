724fe67cd65315d4e1cde0fa0f97d28ac2532b21
kanaugust <kanaugust>
2017-03-03 11:26:58 -0800

    Modified 8e48dd44df2c.R

--- src/8e48dd44df2c.R
+++ src/8e48dd44df2c.R
@@ -1,1 +1,11 @@
-# 
+winsor <- function (x, fraction=.05)
+{
+   if(length(fraction) != 1 || fraction < 0 ||
+         fraction > 0.5) {
+      stop("bad value for 'fraction'")
+   }
+   lim <- quantile(x, probs=c(fraction, 1-fraction))
+   x[ x < lim[1] ] <- lim[1]
+   x[ x > lim[2] ] <- lim[2]
+   x
+}
